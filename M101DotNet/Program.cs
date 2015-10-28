764cd090544c8c901fb5f5e42a390f6e3b8894f2
Tim Marsh <t.marsh@my.westminster.ac.uk>
2015-10-28 22:03:12 +0000

    Update to code for homework 3.1

--- M101DotNet/Program.cs
+++ M101DotNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -10,47 +11,113 @@ namespace M101DotNet
 {
     class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    MainAsync(args).GetAwaiter().GetResult();
+        //    Console.WriteLine();
+        //    Console.WriteLine("Press Enter");
+        //    Console.ReadLine();
+        //}
+
+        //static async Task MainAsync(string[] args)
+        //{
+        //    var connectionString = "mongodb://localhost:27017";
+
+        //    var client = new MongoClient(connectionString);
+
+        //    var db = client.GetDatabase("students");
+        //    var col = db.GetCollection<BsonDocument>("grades");
+
+        //    var filter = new BsonDocument();
+        //    var sort = Builders<BsonDocument>.Sort.Ascending("student_id").Ascending("score");
+
+        //    var list = await col.Find(filter).Sort(sort).ToListAsync();
+
+        //    BsonValue previous_id = "";
+        //    BsonValue student_id = "";
+        //    foreach (var doc in list)
+        //    {
+        //        student_id = doc["student_id"];
+        //        if (student_id != previous_id)
+        //        {
+        //            previous_id = student_id;
+        //            Console.WriteLine(doc);
+
+        //            // Deletes record
+        //            //var result = await col.DeleteOneAsync(doc);
+        //            var col1 = await col.Find(filter).CountAsync();
+        //        }
+        ////            print "Removing", doc
+        ////grades.remove( { '_id': doc['_id'] } )
+
+        ////        Console.WriteLine(doc);
+        //    }
+        //}
+
+        //C:\data\hw>"C:\Program Files\MongoDB\Server\3.0\bin\mongoimport" -d students -c grades<grades.json
+        //C:\data\hw>"C:\Program Files\MongoDB\Server\3.0\bin\mongorestore" --drop --host localhost dump
+
+        // db.grades.aggregate( { '$group' : { '_id' : '$student_id', 'average' : { $avg 
+        // : '$score' }  
+        // } }, { '$sort' : { 'average' : -1 } }, { '$limit' : 1 } )
+
+        // 
+        
+        private static readonly MongoClient _client = new MongoClient();
+
+        private static void Main()
         {
-            MainAsync(args).GetAwaiter().GetResult();
-            Console.WriteLine();
-            Console.WriteLine("Press Enter");
-            Console.ReadLine();
+            try
+            {
+                Do().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
         }
 
-        static async Task MainAsync(string[] args)
+        private static async Task Do()
         {
-            var connectionString = "mongodb://localhost:27017";
+            IMongoDatabase database = _client.GetDatabase("school");
+            IMongoCollection<Item> collection = database.GetCollection<Item>("students");
 
-            var client = new MongoClient(connectionString);
+            List<Item> t = await collection.Find(new BsonDocument()).ToListAsync();
 
-            var db = client.GetDatabase("students");
-            var col = db.GetCollection<BsonDocument>("grades");
-
-            var filter = new BsonDocument();
-            var sort = Builders<BsonDocument>.Sort.Ascending("student_id").Ascending("score");
-
-            var list = await col.Find(filter).Sort(sort).ToListAsync();
-
-            BsonValue previous_id = "";
-            BsonValue student_id = "";
-            foreach (var doc in list)
+            foreach (Item item in t)
             {
-                student_id = doc["student_id"];
-                if (student_id != previous_id)
-                {
-                    previous_id = student_id;
-                    Console.WriteLine(doc);
-
-                    // Deletes record
-                    //var result = await col.DeleteOneAsync(doc);
-                    var col1 = await col.Find(filter).CountAsync();
-                }
-        //            print "Removing", doc
-        //grades.remove( { '_id': doc['_id'] } )
-
-        //        Console.WriteLine(doc);
+                ScoreItem lowerst = item.Scores.OrderBy(x => x.Score).First(x => x.Type == "homework");
+                item.Scores.Remove(lowerst);
+                ReplaceOneResult result = await collection.ReplaceOneAsync(x => x.Id == item.Id, item);
             }
+        }
+
+        [BsonIgnoreExtraElements]
+        private class Item
+        {
+            public int Id { get; set; }
+
+            [BsonElement("scores")]
+            public List<ScoreItem> Scores { get; set; }
+
+            [BsonElement("name")]
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Id: {0}, Scores: {1}", Id, Scores);
+            }
+        }
+
+        private sealed class ScoreItem
+        {
+            [BsonElement("type")]
+            public string Type { get; set; }
+
+            [BsonElement("score")]
+            public double Score { get; set; }
         }
     }
 }
